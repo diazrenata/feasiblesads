12d983d6799e433bea85c971521407abb6a1618a
Renata Diaz <diaz.renatam@gmail.com>
2019-04-11 16:39:23 -0400

    Fixing big int problem

--- R/build_P_table.R
+++ R/build_P_table.R
@@ -1,11 +1,8 @@
 # Recursively build P(s, n) table where P(s, n) is the number of partitions of N into S, including 0s.
 
-library(bit64)
+library(gmp)
 
-max_s = 20
-max_n = 100
-
-fill_ps <- function(max_s, max_n) {
+fill_ps <- function(max_s, max_n, storeyn, storepath) {
 
 ps <- matrix(ncol = max_n + 1, nrow = max_s)
 
@@ -16,6 +13,7 @@ colnames(ps) <- c(0:max_n)
 ps[1, ] = 1 # only one way to put any number of individuals into one species
 ps[, "0"] = 1 # only one way to put 0 individuals into any number of species
 ps[, "1"] = 1 # only one way to put 1 individual into any number of species
+
 
 # ks are the possible values for a given n given number of species and remaining abundance
 kmax <- matrix(ncol = max_n + 1, nrow = max_s)
@@ -30,7 +28,7 @@ for(i in 1:nrow(kmax)) {
 
 p_lookup <- function(k, s, n) {
   this_p = ps[s-1, n - (s*k) + 1]
-  return(as.integer(this_p))
+  return(as.bigz(this_p))
 }
 
 p_over_k <- function(s, n) {
@@ -40,19 +38,25 @@ p_over_k <- function(s, n) {
    this_kmax = kmax[s, n + 1]
 
   for(k in 0:this_kmax) {
-  sum_p = as.integer64(sum_p + p_lookup(k, s, n))
+  sum_p = add.bigz(as.bigz(sum_p), p_lookup(k, s, n))
   }
    return(sum_p)
 }
 
-for(i in 3:ncol(ps)) {
-  for(j in 2:nrow(ps)) {
-    ps[j, i] <- p_over_k(j, i - 1)
+for(i in 2:nrow(ps)) {
+  for(j in 3:ncol(ps)) {
+    ps[i, j] <- as.character(p_over_k(i, j - 1))
   }
 }
 
+if(storeyn) {
+  save(ps, file = paste0(storepath, "p_table.Rds"))
+} else {
 return(ps)
-
+}
 }
 
-system.time(fill_ps(20, 1000))
+system.time(fill_ps(20, 1000, storeyn = TRUE, storepath = 'uniform_fs_sampling/'))
+
+
+load('uniform_fs_sampling/p_table.Rds')
