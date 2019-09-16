6eeb07bb2beaeea3b434a8ba6b30d996d795c77c
Renata Diaz <diaz.renatam@gmail.com>
2019-09-16 11:25:21 -0400

    option to pass p table as argument to sample_fs

--- R/sad_sampler.R
+++ R/sad_sampler.R
@@ -3,26 +3,29 @@
 #' @param s the number of species
 #' @param n the number of individuals
 #' @param nsamples how many samples to draw
-#' @param storeyn TRUE/FALSE whether to store the P table. If FALSE, returns the table.
+#' @param storeyn TRUE/FALSE whether to store the P table.
 #' @param storepath path to store the P table.
-#' @param inpar run in parallel?
+#' @param p_table optionally pass P table as an argument
 #' @return matrix `nsamples` of ranked abundance distributions. Rows are samples and columns are species, sorted from least to most abundant.
 #' @export
 
 sample_fs = function(s, n, nsamples, storeyn = FALSE,
-                     storepath = NULL)
+                     storepath = NULL, p_table = NULL)
 {
+  if(is.null(p_table)) {
     ps <- fill_ps(s, n, storeyn = storeyn, storepath = storepath)
+  } else {
+    ps <- p_table
+  }
+  # Once you have the ps table you also might as well make a ks table? looking up might be as slow as calculating, but whatever.
+  ks <- fill_ks(s, n)
 
-    # Once you have the ps table you also might as well make a ks table? looking up might be as slow as calculating, but whatever.
-    ks <- fill_ks(s, n)
-
-    sets <- matrix(NA, nrow = nsamples, ncol = s)
-    for (idx in seq_len(nsamples))
-    {
-        sets[idx, ] <- send_gnome(s, n, ps, ks)
-    }
-    return(sets)
+  sets <- matrix(NA, nrow = nsamples, ncol = s)
+  for (idx in seq_len(nsamples))
+  {
+    sets[idx, ] <- send_gnome(s, n, ps, ks)
+  }
+  return(sets)
 }
 
 #' @title Draw a single sample from the feasible set
@@ -38,62 +41,62 @@ sample_fs = function(s, n, nsamples, storeyn = FALSE,
 #' @export
 send_gnome <- function(s, n, ps, ks)
 {
-    this_gnome = vector(length = s, mode = 'integer')
+  this_gnome = vector(length = s, mode = 'integer')
 
-    if (s == 1) # if s = 1 there is only one possible RAD.
-    {
-        this_gnome[1] <- n
-        return(this_gnome)
-    }
-
-    slots_remaining = s
-    n_remaining = n
-    current_size <- 0
-    for (species_slot in 1:s)
-    {
-        # final species slot
-        if (species_slot == s) {
-            this_gnome[species_slot] = current_size + n_remaining
-            break
-        }
-
-        if(species_slot == 1) {
-            # first (lowest abundance) value is a boundary case
-            # for the first one, the values of n range from 1 to ks[s, n+1]
-            ns = 1:ks[slots_remaining, n_remaining + 1]
-        } else {
-            # recursive probabilistic sampling
-            # for the rest, the values of n range from
-            # 0 to ks[slots_remaining, n_remaining + 1]
-            ns = 0:ks[slots_remaining, n_remaining+1]
-        }
-
-        # the probability that you select each possible n is
-        # proportional to the # of partitions possible if you
-        # were to choose that n.
-        # divided by the total possible # of partitions across all choices of n.
-        n_parts = ps[slots_remaining - 1,
-                        n_remaining - (slots_remaining * ns) + 1]
-
-        # total partition size
-        total_parts = gmp::sum.bigz(gmp::as.bigz(n_parts))
-
-        # probability of each n choice
-        p_ns = as.numeric(gmp::as.bigq(gmp::as.bigz(n_parts), total_parts))
-
-        # randomly select the next increment:
-        #   update the current_size,
-        #   append it to figure out the current species_slot
-        #   compute the abundance and slots remaining
-        #   save the current increment size
-        this_increment = sample(ns, size = 1, prob = p_ns)
-        current_size <- current_size + this_increment
-        this_gnome[species_slot] = current_size
-        n_remaining = n_remaining - (slots_remaining * this_increment)
-        slots_remaining = slots_remaining - 1
-    }
-
+  if (s == 1) # if s = 1 there is only one possible RAD.
+  {
+    this_gnome[1] <- n
     return(this_gnome)
+  }
+
+  slots_remaining = s
+  n_remaining = n
+  current_size <- 0
+  for (species_slot in 1:s)
+  {
+    # final species slot
+    if (species_slot == s) {
+      this_gnome[species_slot] = current_size + n_remaining
+      break
+    }
+
+    if(species_slot == 1) {
+      # first (lowest abundance) value is a boundary case
+      # for the first one, the values of n range from 1 to ks[s, n+1]
+      ns = 1:ks[slots_remaining, n_remaining + 1]
+    } else {
+      # recursive probabilistic sampling
+      # for the rest, the values of n range from
+      # 0 to ks[slots_remaining, n_remaining + 1]
+      ns = 0:ks[slots_remaining, n_remaining+1]
+    }
+
+    # the probability that you select each possible n is
+    # proportional to the # of partitions possible if you
+    # were to choose that n.
+    # divided by the total possible # of partitions across all choices of n.
+    n_parts = ps[slots_remaining - 1,
+                 n_remaining - (slots_remaining * ns) + 1]
+
+    # total partition size
+    total_parts = gmp::sum.bigz(gmp::as.bigz(n_parts))
+
+    # probability of each n choice
+    p_ns = as.numeric(gmp::as.bigq(gmp::as.bigz(n_parts), total_parts))
+
+    # randomly select the next increment:
+    #   update the current_size,
+    #   append it to figure out the current species_slot
+    #   compute the abundance and slots remaining
+    #   save the current increment size
+    this_increment = sample(ns, size = 1, prob = p_ns)
+    current_size <- current_size + this_increment
+    this_gnome[species_slot] = current_size
+    n_remaining = n_remaining - (slots_remaining * this_increment)
+    slots_remaining = slots_remaining - 1
+  }
+
+  return(this_gnome)
 }
 
 #' @title Filter and tally frequency of distinct RADs
@@ -107,11 +110,11 @@ send_gnome <- function(s, n, ps, ks)
 #' @export
 tally_sets <- function(sets_matrix) {
 
-    sets_matrix <- as.data.frame(sets_matrix) %>%
-        dplyr::group_by_all() %>%
-        dplyr::tally() %>%
-        dplyr::ungroup() %>%
-        dplyr::rename('set_frequency' = 'n')
+  sets_matrix <- as.data.frame(sets_matrix) %>%
+    dplyr::group_by_all() %>%
+    dplyr::tally() %>%
+    dplyr::ungroup() %>%
+    dplyr::rename('set_frequency' = 'n')
 
-    return(sets_matrix)
+  return(sets_matrix)
 }
